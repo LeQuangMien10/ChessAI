b0f101a1e7726c128634191295b391a1020bc1d7
quangnhat09z <160835073+quangnhat09z@users.noreply.github.com>
2025-04-12 20:07:24 +0700

    avoid draw when winning

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ pygame.init()
 screen = pygame.display.set_mode((BOARD_SIZE, BOARD_SIZE))
 pygame.display.set_caption("Chess")
 
+#Test tránh hòa do LẶP NƯỚC 1 (2 xe vs vua)
+fen_string_loop5 = "8/8/8/8/1k6/8/r1r5/K7 w - - 0 1"
+#board = chess.Board(fen_string)
+
+#Test tránh hòa do LẮP NƯỚC 2 (vua vs Mã+Xe)
+fen_string2 = "1k6/8/8/8/8/KN6/8/2R5 w - - 0 1"
+#board = chess.Board(fen_string2)
+
+
 board = chess.Board()
 selected_square = None
 legal_moves = []
@@ -56,7 +65,7 @@ while running:
                     update_screen()
 
                     if not board.is_game_over():
-                        best_move = get_best_move(board, depth=3)
+                        best_move = get_best_move(board, depth=4)
                         if best_move:
                             board.push(best_move)
 
